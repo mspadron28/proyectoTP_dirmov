ebb07c08028f61f3edfb9c0313572d3f79c3cb8d
mspadron28 <matiaspadron2@gmail.com>
2024-10-18 09:31:01 -0500

    Se añade el backend

---
+++ backend/testdirmov-nest/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(process.env.PORT);
+}
+bootstrap();
